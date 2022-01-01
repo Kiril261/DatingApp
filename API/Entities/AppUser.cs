6afdaf9d3fe3ac5c6181061eead1474f80139e31
Kiril Sh <Kiril.shmukler@resolve.io>
2022-01-01 23:37:14 +0200

    End of section 14

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,8 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using API.Extensions;
 
 namespace API.Entities
 {
@@ -24,9 +21,7 @@ namespace API.Entities
         public string Country { get; set; }
         public ICollection<Photo> Photos { get; set; }
 
-        //public int GetAge()
-        //{
-        //    return DateOfBirth.CalculateAge();
-        //}
+        public ICollection<UserLike> LikedByUsers { get; set; }
+        public ICollection<UserLike> LikedUsers { get; set; }
     }
 }
